1301ffe2e62dad47955165ea5e486401f209e7c9
AnkitPatel00 <ankitpatel.stockimage@gmail.com>
2025-02-10 14:40:15 +0530

    address phone field

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/', (req,res) => {
     res.send("Hello Clothing Server")  
 })
 
+// Catch unmatched routes
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Api Route not found' });
+});
+
+
 // ************ JWT Middleware ***********
 
 // jwt auth
